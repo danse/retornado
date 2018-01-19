32ba86d07bcee73e4c1185f394a3c0eb25040696
danse <f.occhipinti@gmail.com>
2018-01-19 20:03:17 +0100

    update the javascript dependency to fix a bug

--- fetch tornado assets .sh
+++ fetch tornado assets .sh
@@ -1,1 +1,1 @@
-export version=0.1; for e in js css; do wget "https://github.com/danse/tornado/raw/${version}/tornado.$e" -O data/tornado.${e}; done
+export version=0.2; for e in js css; do wget "https://github.com/danse/tornado/raw/${version}/tornado.$e" -O data/tornado.${e}; done
